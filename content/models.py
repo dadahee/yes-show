130b23929f07e9b4e54be61b2598ab9e9ad0be86
dadahee <heeouo@kookmin.ac.kr>
2020-07-25 21:38:43 +0900

    settings.py 수정

--- content/models.py
+++ content/models.py
@@ -32,3 +32,7 @@ class Comment(models.Model):
     phone_number = models.CharField(max_length=100)
     text = models.TextField(max_length=200, null=False, blank=False)
     date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{ self.writer }님이 { self.post } 글에 단 댓글"
+    
